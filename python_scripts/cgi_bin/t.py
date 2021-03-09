739abb8eff45069320dce4980da69b96afc1d629
tinup-ku <tinup-ku@users.noreply.github.com>
2021-03-08 23:46:42 -0500

    Update t.py

--- python_scripts/cgi_bin/t.py
+++ python_scripts/cgi_bin/t.py
@@ -1,35 +1,68 @@
 #!/usr/bin/python
 
-# check if file <name>.act is present if yes then run the command , save the output in <name>.act.out
-# remove <name>.act
-
-#import subprocess
-
-#q = subprocess.check_output('/usr/bin/ls /var/tmp')
-#  if ".act" in q:
-#    print "Rar exists"
-
+# http://myserver/cgi-bin/cert.cgi?cert=cert_name
+# Import modules for CGI handling
+import cgi, cgitb
 import os
+import time
 
-mylist = os.listdir('/var/tmp')
-item1=""
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
 
-for i in mylist:
-  #print i
-  if ".act" in i:
-    print "working: " + i
-    #print i
-    x = i.split(".act")
-    item1=x[0]
+# Get data from fields
+cert = form.getvalue('cert')
+#last_name  = form.getvalue('last_name')
 
-    outfile= "/var/tmp/" + item1 + ".out"
-    if os.path.exists(outfile):
-      os.remove(outfile)
-    stream = os.popen('date')
-    output = stream.read()
-    #print outfile
-    f=open(outfile,"w+")
-    f.write(output)
-    f.close()
-    #os.remove(i)
+#create file in /temp(perm 777) since cgi can't create file anywhere else
+newfile="/temp/" + cert + ".act"
+f=open(newfile,"w+")
+f.write("test")
+f.close()
+
+if os.path.exists(newfile):
+  msg="Working on cert: " + cert
+else:
+  msg="Failed to work on: " + cert
+
+outfile= "/temp/" + cert + ".out"
+found="no"
+line1=""
+msg1=""
+n=0
+out=[]
+
+while n < 10 :
+   if os.path.exists(outfile):
+     cont = open(outfile, "r")
+     msg1="Found outfile:"
+     found="yes"
+     for line in cont:
+       out.append(line)
+     os.remove(outfile)
+     break
+   else:
+     time.sleep(1)
+     n=n+1
+     #continue
+
+if found == "no":
+  msg1="Cert clean not successful..."
+
+#os.remove(newfile)
+
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print "<title>Cert clean CGI </title>"
+print "</head>"
+print "<body>"
+print "<h3>%s</h3>" % msg
+#print "<h3>%s</h3>" % msg1
+#print "<h3>%s</h3>" % msg2
+for line in out:
+  print line
+  print "<be>"
+print "</body>"
+print "</html>"
+
 
